4cd0ba8014fed291bf11141390edce8f02ce6417
zevav <zev@averba.ch>
2016-06-10 01:31:50 -0400

    added a list of active dev meetups, and code to iterate over the list instead of passing tuples around, put the event in a namedtuple for easy passing and dot notation made variations of the tweet construction to fit into 140 chars added links and link shortening added time of day information, removed extraneous zeroes

--- _config.py
+++ _config.py
@@ -6,3 +6,14 @@ twitter_token = os.environ['TWITTER_TOKEN']
 twitter_token_secret = os.environ['TWITTER_TOKEN_SECRET']
 
 meetup_key = os.environ['MEETUP_KEY']
+
+meetup_groups = ['VTCode',
+                 'btvpython',
+                 'Vermont-Game-Developers',
+                 'Vermont-VR-AR-Meetup',
+                 'Burlington-Data-Scientists',
+                 'Burlington-Agile-Software-Development-Meetup',
+                 'Burlington-WordPress-Meetup',
+                 'UX-Speakeasy-VT',
+                 'Vermont-Tableau-Users-Group',
+                 'Girl-Develop-It-Burlington']
--- tweet.py
+++ tweet.py
@@ -1,28 +1,52 @@
 #!/usr/bin/env python2.7
 
-from datetime import datetime, timedelta
+from collections import namedtuple
+from datetime import datetime
+import sys
 
 import meetup.api
+from requests.exceptions import ReadTimeout
+from pyshorteners import Shortener
 import pytz
-from pytz import timezone
 from twython import Twython
 
-from config import twitter_key, twitter_secret, twitter_token, \
-	twitter_token_secret, meetup_key
+from _config import twitter_key, twitter_secret, twitter_token, \
+        twitter_token_secret, meetup_key, meetup_groups
 
 
-twitter = Twython(twitter_key, twitter_secret, 
-				  twitter_token, twitter_token_secret)
+twitter = Twython(twitter_key, twitter_secret,
+                  twitter_token, twitter_token_secret)
+meetup_client = meetup.api.Client(meetup_key)
+Event = namedtuple('Event', 'group_name event_name date link')
 
 
-def get_latest_event():
-    client = meetup.api.Client(meetup_key)
-    group = client.GetGroup({"urlname": "VTCode"})
+def get_latest_event(group):
+    group_info = meetup_client.GetGroup({"urlname": group})
+    try:
+        event_info = group_info.next_event
+    except AttributeError:
+        # no upcoming meetups
+        return None
 
-    event_info = group.next_event
-    event_utc = clean_time(event_info["time"])
+    group_name = group_info.name
+    link = make_link(group, event_info['id'])
+    event_name = event_info['name']
+    date = clean_time(event_info['time'])
 
-    return event_info["name"], event_utc
+    return Event(group_name, event_name, date, link)
+
+
+def make_link(group_name, event_id, shorten=True):
+    url = "https://meetup.com/{group_name}/events/{event_id}".format(
+        group_name=group_name,
+        event_id=event_id)
+    if shorten:
+        shortener = Shortener('Tinyurl')
+        try:
+            url =  shortener.short(url)
+        except ReadTimeout:
+            pass
+    return url
 
 
 def clean_time(event_utc):
@@ -30,26 +54,72 @@ def clean_time(event_utc):
     stripped_utc = string_utc[0:10]
 
     utc = pytz.utc
-    fmt = '%m-%d-%Y'
+    fmt = '%m/%d at %I:%M%p'
     utc_dt = utc.localize(datetime.utcfromtimestamp(float(stripped_utc)))
 
-    return utc_dt.strftime(fmt)
+    time =  utc_dt.strftime(fmt).lower()
+    if time[0] == "0":
+        time = time[1:]
+    if time[-7] == "0":
+        time = time[:-7] + time[-6:]
+    if ":00" in time:
+        time = time[:-5] + time[-2:]
+
+    return time
 
 
-def check_for_dupes(tweet):
-    return tweet in [tweet["text"] for tweet in twitter.get_user_timeline()]
+def is_dupe(tweet):
+    return tweet in [_tweet["text"] for _tweet in twitter.get_user_timeline()]
 
 
-def compose_tweet(event_info, event_utc):
-    return "Next up: " + event_info + ": " + event_utc
+def compose_tweet(event):
+    "compose the tweet using whatever info will fit"
+    if tweet_length(event) <= 136:
+
+        tweet = "{group_name}: {event_name} {date} {link}".format(
+                        group_name=event.group_name,
+                        event_name=event.event_name,
+                        date=event.date,
+                        link=event.link)
+
+    elif tweet_length(event, start_at[1]) <= 138:
+
+        tweet = "{event_name} {date} {link}".format(
+                        event_name=event.event_name[:96],
+                        date=event.date,
+                        link=event.link)
+
+    else:
+
+        tweet = "{event_name} {date} {link}".format(
+                        event_name=event.event_name,
+                        date=event.date,
+                        link=event.link)
+
+    return tweet
+
+
+def tweet_length(event, start_at=0):
+    return sum(
+        [len(attrib) for attrib in event.__dict__.values()[start_at:]]
+    )
 
 
 def main():
-    event_name, event_utc = get_latest_event()
-    tweet = compose_tweet(event_name, event_utc)
+    for group in meetup_groups:
+        event = get_latest_event(group)
+        if not event: continue
 
-    if not check_for_dupes(tweet):
+        print("Got an event! {event_name}".format(event_name=event.event_name))
+
+        tweet = compose_tweet(event)
+
+        if is_dupe(tweet):
+            print("Already tweeted that one!")
+            continue
+        print("Tweeting: '" + tweet + "'")
         twitter.update_status(status=tweet)
+
 
 if __name__ == "__main__":
     main()
